74cbcbf5e8be372021e377bf97c9476e5ec96a8d
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-16 17:56:23 +0530

    Create CO23AD0601_5_A1.py

---
+++ CO23AD0601_5_A1.py
@@ -0,0 +1,6 @@
+numberOfPeople = 2390
+print("People at Station A :", numberOfPeople)
+numberOfPeopleAtStationB = numberOfPeople - 239 + 56
+print("People at Station B :", numberOfPeopleAtStationB)
+numberOfPeopleAtStationC = numberOfPeopleAtStationB - 192 + 104
+print("People at Station C :", numberOfPeopleAtStationC)
